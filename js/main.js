8d640931f92ecf3f0e47be58407a051b2a94494a
Ilya <outlow1256@gmail.com>
2025-02-18 13:11:48 +0300

    Created

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// js код
+
 document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burger-menu');
     const menu = document.querySelector('.menu');
